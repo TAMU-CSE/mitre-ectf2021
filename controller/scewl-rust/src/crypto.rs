147d627b21a28f0857098142d9a39995185f2fed
Addison Crump <me@addisoncrump.info>
2021-03-12 12:13:14 -0600

    fix calculation for padding

--- controller/scewl-rust/src/crypto.rs
+++ controller/scewl-rust/src/crypto.rs
@@ -3,6 +3,8 @@ use aes::Aes128;
 use block_modes::block_padding::Pkcs7;
 use block_modes::{BlockMode, Cbc};
 use core::mem::size_of;
+use core::ops::{Add, Rem, Sub};
+use num::Integer;
 use rand::{RngCore, SeedableRng};
 use rand_hc::Hc128Rng;
 
@@ -62,7 +64,7 @@ impl AuthHandler for AESCryptoHandler {
             *to = *from;
         }
         let cbc = Aes128Cbc::new_var(&self.key, &iv).unwrap();
-        let actual = len + 16 + (16 - len % 16);
+        let actual = block_roundup(len);
         cbc.encrypt(&mut data[16..actual], len).unwrap();
 
         actual
@@ -71,12 +73,8 @@ impl AuthHandler for AESCryptoHandler {
     fn decrypt(&mut self, data: &mut [u8; SCEWL_MAX_DATA_SZ], len: usize) -> usize {
         let mut iv = [0_u8; 16];
         iv.copy_from_slice(&data[0..16]);
-        for (to, from) in (0..len).zip(16..(len + 16)) {
-            data[to] = data[from];
-        }
         let cbc = Aes128Cbc::new_var(&self.key, &iv).unwrap();
-        cbc.decrypt(&mut data[16..(len + 16 + (16 - len % 16))])
-            .unwrap();
+        cbc.decrypt(&mut data[16..block_roundup(len)]).unwrap();
         let mut len = [0_u8; size_of::<usize>()];
         for (to, from) in len
             .iter_mut()
@@ -96,4 +94,8 @@ impl AuthHandler for AESCryptoHandler {
         }
         len
     }
+}
+
+fn block_roundup(len: usize) -> usize {
+    len - (len % 16) + 16
 }
