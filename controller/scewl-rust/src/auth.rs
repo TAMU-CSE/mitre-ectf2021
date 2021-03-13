2f7db3b7dfe6a8d1deef47727dd230547887a43e
Addison Crump <me@addisoncrump.info>
2021-03-13 17:55:18 -0600

    Add documentation for auth

--- controller/scewl-rust/src/auth.rs
+++ controller/scewl-rust/src/auth.rs
@@ -1,7 +1,26 @@
+//! Defines the trait for authentication via SSS. This is segmented out in its own module mostly for
+//! visual clarity.
+//!
+//! See [Handler](Handler) for details on how authentication handlers should be defined.
+
 use crate::controller::Controller;
 use crate::crypto::Handler as CryptoHandler;
 
+/// Defines basic methods required for SSS registration and deregistration.
+///
+/// Your implementation should conform to the data and serialisations defined by your `sss.py`
+/// and instantiate an appropriate [encryption handler](crate::crypto::Handler) to handle
+/// cryptographic operations during runtime.
+///
+/// It is expected that your SSS will define values necessary for communications between SEDs.
+/// Ensure that your `sss.py` sufficiently provides any secrets necessary for communications to
+/// the controller so that they may be used by your encryption handler.
 pub trait Handler<C: CryptoHandler + Sized>: Sized + Copy + Clone {
+    /// Register with the SSS. If the registration is successful, it should return a filled [Option](core::option::Option)
+    /// containing the associated [encryption handler](crate::crypto::Handler). If it is not
+    /// successful, the return value should be [None](core::option::Option::None).
     fn sss_register(self, controller: &mut Controller<Self, C>) -> Option<C>;
+
+    /// Deregister with the SSS. Return true if deregistration was successful, false otherwise.
     fn sss_deregister(self, controller: &mut Controller<Self, C>) -> bool;
 }
